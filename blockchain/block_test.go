3cab76ad835ab7200d4d43646aedad69ab0876f5
hhong0326 <hhong0326@naver.com>
2021-10-17 19:36:03 +0900

    Testing Blockchain part 1

--- blockchain/block_test.go
+++ blockchain/block_test.go
@@ -3,6 +3,8 @@ package blockchain
 import (
 	"reflect"
 	"testing"
+
+	"github.com/hhong0326/hhongcoin/utils"
 )
 
 func TestCreateBlock(t *testing.T) {
@@ -17,4 +19,45 @@ func TestCreateBlock(t *testing.T) {
 	if reflect.TypeOf(b) != reflect.TypeOf(&Block{}) {
 		t.Error("createBlock() should return an interface of a block")
 	}
+}
+
+// blockBytes := dbStorage.FindBlock(hash)
+// 	if blockBytes == nil {
+// 		return nil, ErrNotFound
+// 	}
+// 	block := &Block{}
+// 	block.restore(blockBytes)
+// 	return block, nil
+func TestFindBlock(t *testing.T) {
+
+	t.Run("Block not found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				return nil
+			},
+		}
+
+		_, err := FindBlock("x")
+		if err == nil {
+			t.Error("The block should not be found.")
+		}
+	})
+
+	t.Run("Block is found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height: 1,
+				}
+				return utils.ToBytes(b)
+			},
+		}
+
+		block, _ := FindBlock("x")
+
+		if block.Height != 1 {
+			t.Error("Block should be found.")
+		}
+	})
+
 }
--- blockchain/chain_test.go
+++ blockchain/chain_test.go
@@ -1,5 +1,12 @@
 package blockchain
 
+import (
+	"sync"
+	"testing"
+
+	"github.com/hhong0326/hhongcoin/utils"
+)
+
 type fakeDB struct {
 	fakeLoadChain func() []byte
 	fakeFindBlock func() []byte
@@ -16,3 +23,39 @@ func (f fakeDB) LoadChain() []byte {
 func (f fakeDB) SaveBlock(hash string, data []byte) {}
 func (f fakeDB) SaveChain(data []byte)              {}
 func (f fakeDB) EmptyBlocks()                       {}
+
+func TestBlockChain(t *testing.T) {
+
+	t.Run("Should create blockchain", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				return nil
+			},
+		}
+
+		bc := BlockChain()
+		if bc.Height < 1 {
+			t.Error("Blockchain() should create a blockchain")
+		}
+	})
+
+	t.Run("Should restore blockchain", func(t *testing.T) {
+		once = *new(sync.Once) // bcz shared variable in the same package
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				bc := &blockchain{
+					NewestHash:        "x",
+					Height:            2,
+					CurrentDifficulty: 1,
+				}
+
+				return utils.ToBytes(bc)
+			},
+		}
+
+		bc := BlockChain()
+		if bc.Height != 2 {
+			t.Error("Blockchain() should exist a blockchain")
+		}
+	})
+}
